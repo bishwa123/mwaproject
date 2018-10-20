68ce44c4ee361b8d3112c26ead78f65ef23ebf9a
Feven Mamo <fevuhabesha@gmail.com>
2018-10-20 18:54:23 -0500

    add validator

--- backendExamEvaluation/app.js
+++ backendExamEvaluation/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
 
 app.use('/api/v1/questions',apiV1QuestionsRoute);
 app.use('/api/v1/admin',apiV1AdminRoute);
