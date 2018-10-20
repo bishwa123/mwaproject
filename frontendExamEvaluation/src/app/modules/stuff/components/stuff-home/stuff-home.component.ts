f18f485928acfb4c5eac8e69ec0b0843040cb64f
rezkimed17 <rezkimed17@gmail.com>
2018-10-20 13:30:06 -0500

    setting up front end stucture

---
+++ frontendExamEvaluation/src/app/modules/stuff/components/stuff-home/stuff-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stuff-home',
+  templateUrl: './stuff-home.component.html',
+  styleUrls: ['./stuff-home.component.css']
+})
+export class StuffHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontendExamEvaluation/src/app/modules/stuff/stuff.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StuffHomeComponent } from './components/stuff-home/stuff-home.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [StuffHomeComponent]
+})
+export class StuffModule { }
